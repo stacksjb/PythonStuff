4caef5dd2b9a6907b9860b2f9cf5817897b29ccf
Jesse <github@safetacks.info>
2022-12-21 12:11:11 -0700

    Update LogArgTest.py

--- LogArgTest.py
+++ LogArgTest.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-print("LogTest.py")
-
 import logging
 import argparse
 
 def main():
+    print("LogTest.py")
     args = arg_parser()
     log_setup(args)
     LOGGER.debug("Debug message")
