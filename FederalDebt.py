822f63ebecf925ce829c5fe0edc6331bd6fbbdf8
Jesse <github@safetacks.info>
2022-12-21 13:38:08 -0700

    Updating with pre-commit changes

--- FederalDebt.py
+++ FederalDebt.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from pprint import pprint
 import requests
 import json
 import datetime
-#Welcome and print today's date
-print('Welcome to the the current US Federal Debt Tracking App')
-today = datetime.datetime.now().strftime('%Y-%m-%d')
+
+# Welcome and print today's date
+print("Welcome to the the current US Federal Debt Tracking App")
+today = datetime.datetime.now().strftime("%Y-%m-%d")
 print("Today's date is", today)
 # Get the data from the API
-response = requests.get('https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?fields=record_date,debt_held_public_amt,intragov_hold_amt,tot_pub_debt_out_amt&page[size]=1&sort=-record_date')
-#load the JSON response
+response = requests.get(
+    "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?fields=record_date,debt_held_public_amt,intragov_hold_amt,tot_pub_debt_out_amt&page[size]=1&sort=-record_date"
+)
+# load the JSON response
 r = json.loads(response.content)
-record_date=r['data'][0]['record_date']
-#Parse values into a float
-debt_held_public_amt=float(r['data'][0]['debt_held_public_amt'])
-intragov_hold_amt=float(r['data'][0]['intragov_hold_amt'])
-tot_pub_debt_out_amt=float(r['data'][0]['tot_pub_debt_out_amt'])
+record_date = r["data"][0]["record_date"]
+# Parse values into a float
+debt_held_public_amt = float(r["data"][0]["debt_held_public_amt"])
+intragov_hold_amt = float(r["data"][0]["intragov_hold_amt"])
+tot_pub_debt_out_amt = float(r["data"][0]["tot_pub_debt_out_amt"])
 # Population data
-response = requests.get("https://api.census.gov/data/2021/pep/natmonthly?get=POP,NAME&for=us:*")
+response = requests.get(
+    "https://api.census.gov/data/2021/pep/natmonthly?get=POP,NAME&for=us:*"
+)
 r = json.loads(response.content)
 # example response:
 # [['POP', 'NAME', 'us'], ['331893745', 'United States', '1']]
 us_population = int(r[1][0])
-debt_per_person=tot_pub_debt_out_amt/us_population
-#Print the values
+debt_per_person = tot_pub_debt_out_amt / us_population
+# Print the values
 print("Retrieved the following data from the API: ")
-print('Record Date: ', record_date)
-#Use printf to format the output to $ with commas
-print('Debt Held by the Public $', end='')
+print("Record Date: ", record_date)
+# Use printf to format the output to $ with commas
+print("Debt Held by the Public $", end="")
 print(f"{(debt_held_public_amt):,.2f}")
-print('Intragovernmental Holdings $', end='')
+print("Intragovernmental Holdings $", end="")
 print(f"{(intragov_hold_amt):,.2f}")
-print('Total Public Debt Outstanding $', end='')
+print("Total Public Debt Outstanding $", end="")
 print(f"{(tot_pub_debt_out_amt):,.2f}")
-print('Total Public Debt Per Person $', end='')
+print("Total Public Debt Per Person $", end="")
 print(f"{(debt_per_person):,.2f}")
-print('Thank you for using the current US Federal Debt Tracking App')
+print("Thank you for using the current US Federal Debt Tracking App")
--- LogArgTest.py
+++ LogArgTest.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # last_modified_at: 2022-01-01 00:00:00
 
 import logging
 import argparse
+
 
 def main():
     print("LogTest.py")
@@ -15,27 +17,48 @@ def main():
     LOGGER.error("Error message")
     LOGGER.critical("Critical message")
 
-#Set up Argument Parser
+
+# Set up Argument Parser
 def arg_parser():
     parser = argparse.ArgumentParser(description="A test of logging and argparse")
-    parser.add_argument("-a", "--auto", help="Automatically sync previously set courses", action="store_true")
+    parser.add_argument(
+        "-a",
+        "--auto",
+        help="Automatically sync previously set courses",
+        action="store_true",
+    )
     parser.add_argument("-l", "--log", help="Log output to file", action="store_true")
-    parser.add_argument('-v', '--verbose', action="count", help="Verbosity level: v=error, vv=warning, vvv=info, vvvv=debug (default: critical)", default=0)
-    args=parser.parse_args()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        help="Verbosity level: v=error, vv=warning, vvv=info, vvvv=debug (default: critical)",
+        default=0,
+    )
+    args = parser.parse_args()
     return args
 
-#Set up logging functions and set level
+
+# Set up logging functions and set level
 def log_setup(args):
     global LOGGER
     LOGGER = logging.getLogger(__name__)
-    log_level = [logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG][args.verbose]
+    log_level = [
+        logging.CRITICAL,
+        logging.ERROR,
+        logging.WARNING,
+        logging.INFO,
+        logging.DEBUG,
+    ][args.verbose]
     LOGGER.setLevel(log_level)
 
     # create formatter and add it to the handlers
     # Uses time.strftime(format[, t]), see
     # https://docs.python.org/3/library/time.html#time.strftime
-    
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     # create console handler for iostream.
     ch = logging.StreamHandler()
     ch.setLevel(log_level)
@@ -43,16 +66,15 @@ def log_setup(args):
     # add the handlers to the logger
     LOGGER.addHandler(ch)
 
-    if args.log: #Setup file handler if arg is set
+    if args.log:  # Setup file handler if arg is set
         # create file handler that logs at configured log_level to file 'run.log'
-        fh = logging.FileHandler('run.log')
+        fh = logging.FileHandler("run.log")
         fh.setLevel(log_level)
         fh.setFormatter(formatter)
         LOGGER.addHandler(fh)
-  
-    if args.auto: #Log if auto is set
-        LOGGER.info("Auto is set")
 
+    if args.auto:  # Log if auto is set
+        LOGGER.info("Auto is set")
 
 
 if __name__ == "__main__":
